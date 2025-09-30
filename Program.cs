1a02c8ecda19c4b122d4471caa944944cc94ecd9
Hiran Ferreti Baccos <hiranjc1@outlook.com>
2025-09-30 18:25:49 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Generics1ReusoTypeSafetyPerformance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintService<int> printService = new PrintService<int>();
+            Console.Write("How many values? ");
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                printService.AddValue(x);
+            }
+
+            printService.Print();
+            Console.WriteLine("First: " + printService.First());
+        }
+    }
+}
